9735634b892f5027273128c575c42a34ad827c79
Ganesh-Kumatole <workspace.ganeshkumatole@gmail.com>
2025-12-01 15:51:07 +0000

    refactor: create custom hooks for data-fetching

--- src/components/layouts/JobDetails.jsx
+++ src/components/layouts/JobDetails.jsx
@@ -1,16 +1,13 @@
 import { useParams, Link } from 'react-router';
-import jobsJSON from '../../jobs.json';
+import { ClipLoader } from 'react-spinners';
+import useFetchJob from '../../hooks/useFetchJob';
 
 const JobDetails = () => {
+  // Obtain the jobID
   const { id } = useParams();
-  let clickedJob;
 
-  for (const job of jobsJSON.jobs) {
-    if (job.id === id) {
-      clickedJob = job;
-      break;
-    }
-  }
+  // Obtain specific job-details
+  const { job, isFetching } = useFetchJob(id);
 
   return (
     <>
@@ -29,70 +26,82 @@ const JobDetails = () => {
       <section className="bg-indigo-50">
         <div className="container m-auto py-10 px-6">
           <div className="flex justify-center items-start gap-3">
-            <main className="w-7/10">
-              <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-                <div className="text-gray-500 mb-4">{clickedJob.type}</div>
-                <h1 className="text-3xl font-bold mb-4">{clickedJob.title}</h1>
-                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                  <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-                  <p className="text-orange-700">{clickedJob.location}</p>
-                </div>
-              </div>
+            {isFetching ? (
+              <ClipLoader
+                color="indigo"
+                loading={isFetching}
+                size={75}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            ) : (
+              <>
+                <main className="w-7/10">
+                  <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                    <div className="text-gray-500 mb-4">{job.type}</div>
+                    <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
+                    <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                      <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+                      <p className="text-orange-700">{job.location}</p>
+                    </div>
+                  </div>
 
-              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                  Job Description
-                </h3>
+                  <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                    <h3 className="text-indigo-800 text-lg font-bold mb-6">
+                      Job Description
+                    </h3>
 
-                <p className="mb-4">{clickedJob.description}</p>
+                    <p className="mb-4">{job.description}</p>
 
-                <h3 className="text-indigo-800 text-lg font-bold mb-2">
-                  Salary
-                </h3>
+                    <h3 className="text-indigo-800 text-lg font-bold mb-2">
+                      Salary
+                    </h3>
 
-                <p className="mb-4">{clickedJob.salary}</p>
-              </div>
-            </main>
+                    <p className="mb-4">{job.salary}</p>
+                  </div>
+                </main>
 
-            {/* <!-- Sidebar --> */}
-            <aside className="w-3/10">
-              {/* <!-- Company Info --> */}
-              <div className="bg-white p-6 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-6">Company Info</h3>
+                {/* <!-- Sidebar --> */}
+                <aside className="w-3/10">
+                  {/* <!-- Company Info --> */}
+                  <div className="bg-white p-6 rounded-lg shadow-md">
+                    <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                <h2 className="text-2xl">{clickedJob.company.name}</h2>
+                    <h2 className="text-2xl">{job.company.name}</h2>
 
-                <p className="my-2">{clickedJob.company.description}</p>
+                    <p className="my-2">{job.company.description}</p>
 
-                <hr className="my-4" />
+                    <hr className="my-4" />
 
-                <h3 className="text-xl">Contact Email:</h3>
+                    <h3 className="text-xl">Contact Email:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {clickedJob.company.contactEmail}
-                </p>
+                    <p className="my-2 bg-indigo-100 p-2 font-bold">
+                      {job.company.contactEmail}
+                    </p>
 
-                <h3 className="text-xl">Contact Phone:</h3>
+                    <h3 className="text-xl">Contact Phone:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {clickedJob.company.contactPhone}
-                </p>
-              </div>
+                    <p className="my-2 bg-indigo-100 p-2 font-bold">
+                      {job.company.contactPhone}
+                    </p>
+                  </div>
 
-              {/* <!-- Manage --> */}
-              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                <a
-                  href="/add-job.html"
-                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >
-                  Edit Job
-                </a>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
-                  Delete Job
-                </button>
-              </div>
-            </aside>
+                  {/* <!-- Manage --> */}
+                  <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                    <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+                    <a
+                      href="/add-job.html"
+                      className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                    >
+                      Edit Job
+                    </a>
+                    <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                      Delete Job
+                    </button>
+                  </div>
+                </aside>
+              </>
+            )}
           </div>
         </div>
       </section>
---
+++ src/hooks/useFetchJob.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+function useFetchJob(id) {
+  // State to store job-details
+  const [job, setJob] = useState({});
+  const [isFetching, setIsFetching] = useState(true);
+
+  // Job-Details Data fetching
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/jobs/${id}`);
+        if (!response.ok) throw new Error('Network Request Failed...');
+        const data = await response.json();
+        setJob(data);
+        setIsFetching(false);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    fetchJob();
+  }, [id]);
+
+  return { job, isFetching };
+}
+
+export default useFetchJob;
